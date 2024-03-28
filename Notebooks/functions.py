a62e94db3880124d18c4b1280cde1376ea97bea6
Gv1as <gijsverbree@gmail.com>
2024-03-28 22:44:12 +0100

    Push updates containing chart info

---
+++ Notebooks/functions.py
@@ -0,0 +1,25 @@
+def calculate_total_times(df):
+    finish_df = df[df['process_step'] == "confirm"]
+    start_df = df[df['process_step'] == "start"]
+
+    total_times = []
+
+    for visit_id in finish_df['visit_id_numbers'].unique():
+        finish_time = finish_df[(finish_df['visit_id_numbers'] == visit_id)].iloc[-1]['date_time']
+
+        start_times_before_confirm = start_df[(start_df['visit_id_numbers'] == visit_id) & (start_df['date_time'] < finish_time)]
+        if not start_times_before_confirm.empty:
+            starting_time = start_times_before_confirm.iloc[-1]['date_time']
+            total_time = finish_time - starting_time
+            total_times.append(total_time)
+
+    return total_times
+
+def calculate_time_differences_step(test_finish_df, first_step, next_step):
+    time_differences = []
+    for client_id, group in test_finish_df.groupby('visit_id_numbers'):
+        for i in range(len(group) - 1):
+            if group.iloc[i]['step_value'] == first_step and group.iloc[i + 1]['step_value'] == next_step:
+                time_diff = group.iloc[i + 1]['date_time'] - group.iloc[i]['date_time']
+                time_differences.append(time_diff.total_seconds())
+    return time_differences
